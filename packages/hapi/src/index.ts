615ce019dc76d684359b8bbae12b39efe07ec9dd
arsaizdihar <arsadihar@gmail.com>
2025-05-18 18:08:40 +0700

    feat: proper routing for hapi

--- packages/hapi/src/index.ts
+++ packages/hapi/src/index.ts
@@ -1,24 +1,84 @@
 import { RouteFactory, RouteConfig } from '@node-openapi/core';
 import { HapiRequestAdapter } from './request';
 import { OpenAPIObjectConfigV31 } from '@asteasolutions/zod-to-openapi/dist/v3.1/openapi-generator';
-import { Server, Request, ResponseToolkit, ServerRoute } from '@hapi/hapi';
-import { ZodError } from 'zod';
+import {
+  Request,
+  ResponseToolkit,
+  ServerRoute,
+  Plugin,
+  Server,
+} from '@hapi/hapi';
 export { z } from '@node-openapi/core';
+
+type InternalPluginOptions = {
+  middlewares?: Array<(request: Request, h: ResponseToolkit) => Promise<any>>;
+};
 
 export class HapiRouteFactory extends RouteFactory<HapiRequestAdapter> {
   private readonly _middlewares: Array<
     (request: Request, h: ResponseToolkit) => Promise<any>
   > = [];
 
-  constructor(private readonly server: Server) {
+  private plugin: Plugin<InternalPluginOptions, unknown>;
+  private children: { path: string; factory: HapiRouteFactory }[] = [];
+  private routes: ServerRoute[] = [];
+
+  constructor(name?: string) {
     super();
+
+    this.plugin = {
+      name:
+        name || 'node-openapi-' + Math.random().toString(36).substring(2, 15),
+      register: (server, options) => {
+        for (const route of this.routes) {
+          let routeOptions = route.options;
+          if (typeof routeOptions === 'function') {
+            routeOptions = routeOptions(server);
+          }
+
+          server.route({
+            ...route,
+            options: {
+              ...routeOptions,
+              pre: [
+                // middlewares from parent plugin
+                ...(options?.middlewares || []),
+                // middlewares from this plugin
+                ...(routeOptions?.pre || []),
+              ],
+            },
+          });
+        }
+
+        for (const child of this.children) {
+          server.register(
+            {
+              plugin: child.factory.plugin,
+              options: {
+                middlewares: [
+                  // middlewares from parent plugin
+                  ...(options?.middlewares || []),
+                  // middlewares from this plugin
+                  ...this._middlewares,
+                ],
+              },
+            },
+            {
+              routes: {
+                prefix: child.path,
+              },
+            },
+          );
+        }
+      },
+    };
   }
 
   middleware(handler: (request: Request, h: ResponseToolkit) => Promise<any>) {
     this._middlewares.push(handler);
   }
 
-  route<R extends RouteConfig>(
+  route<R extends RouteConfig & { getRoutingPath: () => string }>(
     route: R,
     handler: (request: Request, h: ResponseToolkit) => Promise<any>,
   ) {
@@ -30,42 +90,34 @@ export class HapiRouteFactory extends RouteFactory<HapiRequestAdapter> {
 
     const serverRoute: ServerRoute = {
       method: route.method,
-      path: route.path,
+      path: route.getRoutingPath(),
+      options: {
+        pre: this._middlewares,
+      },
       handler: async (request, h) => {
         const context = {
           req: new HapiRequestAdapter(request),
           input: {},
         };
 
-        try {
-          // Run middlewares
-          for (const middleware of this._middlewares) {
-            await middleware(request, h);
-          }
-
-          const c = await _route(context);
-          const input = c.input as any;
-          (request as any).input = input;
-          return handler(request, h);
-        } catch (error) {
-          if (error instanceof ZodError) {
-            return h.response({ error }).code(400);
-          }
-          return h.response({ message: 'Internal Server Error' }).code(500);
-        }
+        const c = await _route(context);
+        const input = c.input as any;
+        (request as any).input = input;
+        return handler(request, h);
       },
     };
 
-    this.server.route(serverRoute);
+    this.routes.push(serverRoute);
   }
 
   router(path: string, routeFactory: HapiRouteFactory) {
+    this.children.push({ path, factory: routeFactory });
     const pathForOpenAPI = path.replaceAll(/:([^/]+)/g, '{$1}');
     this._registerRouter(pathForOpenAPI, routeFactory);
   }
 
   doc<P extends string>(path: P, configure: OpenAPIObjectConfigV31) {
-    this.server.route({
+    this.routes.push({
       method: 'get',
       path,
       handler: (_, h) => {
@@ -77,6 +129,10 @@ export class HapiRouteFactory extends RouteFactory<HapiRequestAdapter> {
         }
       },
     });
+  }
+
+  registerServer(server: Server) {
+    server.register(this.plugin);
   }
 }
 
